bd897b859f11e7c8020ec4f6717d40bda57bc676
ashwith2427 <87614279+ashwith2427@users.noreply.github.com>
2022-03-20 17:53:58 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -6,7 +6,6 @@ import * as dat from 'dat.gui'
 const gui = new dat.GUI()
 
 const textureloader = new THREE.TextureLoader()
-const normaltexture = textureloader.load('/textures/NormalMap.png')
     // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -14,33 +13,33 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.SphereBufferGeometry(.5, 64, 64)
+const geometry = new THREE.SphereBufferGeometry(1, 64, 64)
 
 // Materials
 
-const material = new THREE.MeshStandardMaterial()
-material.color = new THREE.Color(0x292929)
-material.normalMap = normaltexture;
-material.roughness = 0.2
-material.metalness = 0.2
+const material = new THREE.MeshStandardMaterial({
+    color: 0x292929,
+    normalMap: textureloader.load(`/textures/nm0.png`),
+    roughness: 0.18,
+    metalness: 0.7,
+})
 
-
-// Mesh
 const sphere = new THREE.Mesh(geometry, material)
 scene.add(sphere)
+    // Mesh
 
 // Lights
 
 const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
+pointLight.position.x = 3
 pointLight.position.y = 3
 pointLight.position.z = 5
 scene.add(pointLight)
-const pointlight2 = new THREE.PointLight(0xff0000, 0.6)
+const pointlight2 = new THREE.PointLight(0xff0000, 1)
 pointlight2.position.set(-2, 1, 1)
 scene.add(pointlight2)
-const pointlight4 = new THREE.PointLight(0xFFD700, 0.5)
-pointlight4.position.set(1, -1, 3)
+const pointlight4 = new THREE.PointLight(0xFFD700, 1)
+pointlight4.position.set(1, -1, 1)
 scene.add(pointlight4)
 gui.add(pointlight4.position, 'x').min(-4).max(4).step(0.02)
 gui.add(pointlight4.position, 'y').min(-4).max(4).step(0.02)
@@ -96,7 +95,11 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-
+let y = 0
+let position = 0
+window.addEventListener('wheel', (event) => {
+    y = (event.deltaY) * 0.007
+})
 const clock = new THREE.Clock()
 document.addEventListener('mousemove', onDocumentmove)
 let targetX = 0
@@ -124,8 +127,10 @@ const tick = () => {
     sphere.rotation.z += .5 * (targetY - sphere.rotation.x)
         // Render
     renderer.render(scene, camera)
-
-    // Call tick again on the next frame
+    position += y
+    y = y * 0.0007
+    camera.position.y = position
+        // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
